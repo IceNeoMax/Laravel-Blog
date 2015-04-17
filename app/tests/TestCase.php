fca6a62b354503178feb894cf0b47ce73d47b24d
IceNeoMax <study4fun2011@gmail.com>
2015-04-17 22:42:20 +0700

    ồi

--- app/tests/TestCase.php
+++ app/tests/TestCase.php
@@ -20,6 +20,6 @@ class TestCase extends Illuminate\Foundation\Testing\TestCase {
 
 		return require __DIR__.'/../../bootstrap/start.php';
 	}
-	//Demo de up len git
+	//Demo de up len gi
 	//Nhuan add
 }
